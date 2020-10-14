972d110225d0585b7701ad5f3fd04106fbb85a52
alekseybogdanchik <abp163@mail.ru>
2020-10-14 16:04:51 +0400

    1. Симуляция терминала Linux

---
+++ chapter_002/src/main/java/ru/job4j/inout/Shell.java
@@ -0,0 +1,42 @@
+package ru.job4j.inout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Shell {
+
+    private List<String> curDir = new ArrayList<>();
+
+    public void cd(String path) {
+        if (path.startsWith("/")) {
+            curDir.clear();
+        }
+        if (path.contains("/") && path.length() > 1) {
+            String[] tempDir = path.split("/");
+            for (String s : tempDir) {
+                if (!s.equals("") && !s.equals("..")) {
+                    curDir.add(s);
+                }
+            }
+        } else if (!path.equals("..") && !path.equals("/")) {
+            curDir.add(path);
+        }
+        if (path.contains("..") && curDir.size() != 0) {
+            curDir.remove(curDir.size() - 1);
+        }
+    }
+
+    public String pwd() {
+        StringBuilder result = new StringBuilder();
+        if (curDir.size() == 0) {
+            result.append("/");
+        } else {
+            for (String dir : curDir) {
+                result.append("/");
+                result.append(dir);
+            }
+        }
+        return result.toString();
+    }
+}
